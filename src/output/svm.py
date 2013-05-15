83a49730646b5085a003440d1288fcebac331de8
Samuel Messing <samuel.messing@gmail.com>
2013-05-15 10:24:25 -0400

    Making names a bit more generic.

--- src/output/svm.py
+++ src/output/svm.py
@@ -24,7 +24,7 @@ def write_species_training_file(
 
     negative_examples: a dictionary of { species_name : numpy.ndarray }
         (where arrays are size D x N) representing all of the species to be
-        treated as noise. These examples will be labeled with the NO_BIRD
+        treated as noise. These examples will be labeled with the NO_SPECIES
         label.
 
     training_data: a dictionary of { species_name : numpy.ndarray }.
@@ -38,7 +38,7 @@ def write_species_training_file(
     ICML_BIRD_MODEL_PATH/model_name/train.dat that can be used by svm_hmm_learn
     to train the model.
   """
-  out_file = _get_training_file(model_name, opt_training_filename)
+  out_file = _get_out_file(model_name, opt_training_filename)
   print 'Writing examples to %s...' % out_file.name
   try:
 
@@ -46,13 +46,13 @@ def write_species_training_file(
 
     for species in positive_examples:
       label = names.get_index_for_species(species)
-      _write_training_example(
+      _write_example(
           example_num, label, positive_examples[species], out_file, species)
       example_num += 1
 
     for species in negative_examples:
       label = names.get_index_for_species(names.NO_SPECIES)
-      _write_training_example(
+      _write_example(
           example_num, label, negative_examples[species], out_file, species)
       example_num += 1
 
@@ -62,9 +62,9 @@ def write_species_training_file(
   print '...done'
 
 
-def _write_training_example(index, label, frames, out_file, opt_comment=""):
+def _write_example(index, label, frames, out_file, opt_comment=""):
   """
-    Write an example to the training file.
+    Write an example to a file.
 
     index - The number to use to identify this example.
     label - The numeric label for the sequence.
@@ -81,18 +81,18 @@ def _write_training_example(index, label, frames, out_file, opt_comment=""):
     out_file.write(string)
 
 
-def _get_training_file(model_name, training_filename):
+def _get_out_file(model_name, training_filename):
   """
     Handle generating / opening the training file for this model.
 
     model_name - The name (string) of the model to create / open the training
                  file for.
   """
-  filename = _make_training_filename(model_name, training_filename)
+  filename = _make_out_filename(model_name, training_filename)
   return files.ensure_file_exists_and_open(filename)
 
 
-def _make_training_filename(model_name, training_filename):
+def _make_out_filename(model_name, training_filename):
   """
     Transform a model name into its training filename.
 
@@ -109,13 +109,43 @@ def _make_training_filename(model_name, training_filename):
   return out_filename
 
 
-def write_testing_file(testing_data, model_name):
+def write_testing_file(
+    testing_data,
+    model_name,
+    opt_testing_filename="test.dat"):
   """
     Write a .dat testing file for use with svm_hmm_classify.
 
+    testing_data: A dictionary of {time_and_location : numpy.ndarray} (where
+        arrays are size D x N) representing all of the testing recordings to
+        be labeled. These examples will be labeled in the test file as
+        names. NO_SPECIES since we don't know the ground truth.
+
     model_name: a unique string to use to identify this model.
+
+    opt_testing_filename: optinal filename to use for testing file. If not
+        specified, defaults to "test.dat"
   """
-  pass
+  out_file = _get_out_file(model_name, opt_testing_filename)
+  print 'Writing testing file: %s...' % out_file.name
+  try:
+
+    example_num = 1
+
+    for time_and_location in testing_data:
+      label = names.NO_SPECIES_KEY
+      _write_example(
+          example_num,
+          label,
+          testing_data[time_and_location],
+          out_file,
+          time_and_location)
+      example_num  += 1
+
+  finally:
+    out_file.close()
+
+  print '...done'
 
 
 def write_set_to_label(label_data, model_name):
